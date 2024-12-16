9232aeb857181920486511ac90b25f9a08f44fd2
liujohnson118 <geyi.liu@outlook.com>
2024-12-16 12:28:30 -0700

    fix

--- lib/graphql_user_web/schema/subscriptions/user.ex
+++ lib/graphql_user_web/schema/subscriptions/user.ex
@@ -2,16 +2,24 @@ defmodule GraphqlUserWeb.Schema.Subscriptions.User do
   use Absinthe.Schema.Notation
 
   object :user_subscriptions do
-    field :user_created, :user do
-      trigger :create_user, topic: fn params ->
-        IO.puts "----------------------"
-        IO.inspect params
-        IO.puts "----------------------"
+    field :created_user, :user do
+      trigger :create_user, topic: fn _ ->
         "new_user"
       end
 
       config fn _, _ ->
         {:ok, topic: "new_user"}
+      end
+    end
+
+    field :updated_user_preferences, :preferences do
+      arg :user_id, non_null(:id)
+      trigger :update_user_preferences, topic: fn _ ->
+        "updated_user_preferences"
+      end
+
+      config fn _,_ ->
+        {:ok, topic: "updated_user_preferences"}
       end
     end
   end

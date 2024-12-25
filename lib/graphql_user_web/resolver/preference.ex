4d9a3c59adc18b4f592eb186980822eb3fe3371c
liujohnson118 <geyi.liu@outlook.com>
2024-12-25 13:35:04 -0700

    update

--- lib/graphql_user_web/resolver/preference.ex
+++ lib/graphql_user_web/resolver/preference.ex
@@ -1,7 +1,14 @@
 defmodule GraphqlUserWeb.Resolver.Preference do
   alias GraphqlUser.Accounts
+  alias GraphqlUser.Accounts.Preference
+  alias GraphqlUser.Repo
 
   def update(%{id: id} = params, _) do
     Accounts.update_preference(id, Map.delete(params, :id))
+  end
+
+  def update_user_preference(%{user_id: user_id} = params, _) do
+    preference_id = Repo.get_by(Preference, user_id: user_id).id
+    Accounts.update_preference(preference_id, Map.delete(params, :user_id))
   end
 end
--- lib/graphql_user_web/schema/mutations/preference.ex
+++ lib/graphql_user_web/schema/mutations/preference.ex
@@ -10,5 +10,14 @@ defmodule GraphqlUserWeb.Schema.Mutations.Preference do
 
       resolve &GraphqlUserWeb.Resolver.Preference.update/2
     end
+
+    field :update_user_preference, :preference do
+      arg :user_id, non_null(:id)
+      arg :likes_emails, :boolean
+      arg :likes_phone_calls, :boolean
+      arg :likes_faxes, :boolean
+
+      resolve &GraphqlUserWeb.Resolver.Preference.update_user_preference/2
+    end
   end
 end
